c4254efa80fba1c26c03e29f327eb36f2e79b52f
Razi Alexis <razialexis43@gmail.com>
2017-03-07 11:30:21 +0200

    Rewrite subiectul 1, admitere iulie 2016

--- AdmitereUBB/ubb_admitere_iulie_2016/sub1.cpp
+++ AdmitereUBB/ubb_admitere_iulie_2016/sub1.cpp
@@ -4,163 +4,99 @@
 
 using namespace std;
 
-void SubI_1(int n)
+void determina(int p, int q, int n, int &k, int *x)
 {
-    int v[100];
-    int marime = 1;
-    int n2 = n - 1;
-    v[0] = 1;
-    int s = 1;
-    while (n2 > 1)
+    bool magic;
+    for (int i = 1; i < n; ++i)
     {
-        v[marime++] = n2;
-        v[marime++] = 1;
+        magic = true;
+        int vdf[10] = {};
+        int nr = i;
+        while (nr > 0)
+        {
+            vdf[nr % p] = 1;
+            nr /= p;
+        }
+        nr = i;
+        while (nr > 0)
+        {
+            ++vdf[nr % q];
+            nr /= q;
+        }
 
-        s += 1 + n2;
-        --n2;
-    }
+        for (int j = 0; j < 10; ++j)
+            if (vdf[j] == 1)
+            {
+                magic = false;
+                break;
+            }
 
-        cout << 2 * marime + 1 << " turnuri; " << 2 * s + n << " monede";
-}
-
-// ultima cifra a lui a^n
-int F(int a, int b)
-{
-    int c = 1;
-    while (b > 0)
-    {
-        if (b % 2 == 1)
-            c = (c * a) % 10;
-
-        a = (a * a) % 10;
-        b /= 2;
-    }
-
-    return c;
-}
-
-int F_REC(int a, int b)
-{
-    if (b == 0)
-        return a;
-
-    if (b % 2 == 1)
-        return F_REC(a % 10, b / 2); // nu e corecta linia
-    else
-        return F_REC((a * a) % 10, b / 2);
-}
-
-void SubI_3(int a[], int &n)
-{
-    int i = 1;
-    int nr_de_pus;
-
-    while (i <= n)
-    {
-        nr_de_pus = 0;
-
-        if (a[i] == 1 || a[i] == 2)
-            nr_de_pus = 1;
-        else
-            for (int k = 2; k < a[i]; k *= 2)
-                if (k > nr_de_pus)
-                    nr_de_pus = k;
-
-        for (int p = n; p > i; --p)
-            a[p + 1] = a[p];
-
-        a[i + 1] = nr_de_pus;
-        ++n;
-        i += 2;
+        if (magic)
+            x[k++] = i;
     }
 }
 
-void SubI_2(int p, int q, int x[], int &k)
+// n == 3: 1 2 1 3 1 2 1
+void turnuri(int n)
 {
+    int s = 0   , cnt = 0;
+    for (int i = 1; i < n; ++i)
+    {
+        ++s;
+        ++cnt;
+    }
+    for (int i = 2; i < n; ++i)
+    {
+        s += i;
+        ++cnt;
+    }
 
+    cout << 2 * cnt + 1 << " turnuri si " << 2 * s + n << " monede";
+}
+
+void inserare(int &n, int *v)
+{
+    int nr;
+    for (int i = n - 1; i >= 0; --i)
+    {
+        nr = 1;
+        while (2 * nr < v[i])
+            nr *= 2;
+
+        v[2 * i] = v[i];
+        v[2 * i + 1] = nr;
+    }
+
+    n *= 2;
 }
 
 int main()
 {
-    int a, b;
-    cin >> a >> b;
-    cout << F(a, b) << endl << F_REC(a, b);
     /*
-    int x[100] = {}, k = 0, p, q;
-    cin >> p >> q;
+    int n;
+    cin >> n;
+    turnuri(n);
+    */
 
-    int n, n1, b, aux, nr;
-    bool ok;
+    /*
+    int b1, b2, n, k = 0, x[100];
+    cin >> b1 >> b2 >> n;
+    determina(b1, b2, n, k, x);
 
-    aux = nr = 0;
-    int nr2;
+    for (int i = 0; i < k; ++i)
+        cout << x[i] << ' ';
+    */
 
-    //cin >> n;
-    int limita;
-    cin >> limita;
+    int n, v[100];
+    cin >> n;
 
-    for (int i = 1; i <= limita; ++i)
-    {
-        ok = true;
-        n = i;
-        b = p;
-        n1 = n;
+    for (int i = 0; i < n; ++i)
+        cin >> v[i];
 
-    do
-    {
-        aux = aux * 10 + n1 % b;
-        n1 /= b;
-    } while (n1);
+    inserare(n, v);
 
-    do
-    {
-        nr = nr * 10 + aux % 10;
-        aux /= 10;
-    } while (aux);
+    for (int i = 0; i < n; ++i)
+        cout << v[i] << ' ';
 
-    nr2 = nr;
-    //cout << nr << endl;
-
-    b = q;
-    n1 = n;
-
-    do
-    {
-        aux = aux * 10 + n1 % b;
-        n1 /= b;
-    } while (n1);
-
-    do
-    {
-        nr = nr * 10 + aux % 10;
-        aux /= 10;
-    } while (aux);
-
-    //cout << nr << endl;
-
-        int vdf1[10] = {}, vdf2[10] = {};
-
-        //cout << nr << ' ' << nr2 << endl;
-        while (nr)
-        {
-            vdf1[nr % 10] = 1;
-            nr /= 10;
-        }
-
-        while (nr2)
-        {
-            vdf2[nr2 % 10] = 1;
-            nr2 /= 10;
-        }
-
-        for (int k = 0; k < 10; ++k)
-            if (vdf1[k] != vdf2[k])
-            {
-                ok = false;
-                break;
-            }
-
-        if (ok)
-            cout << i << " ESTE\n";
-    }*/
+    return 0;
 }
