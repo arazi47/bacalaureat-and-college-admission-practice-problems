834043328e40e1ec43f2d32415d922d87d9815ef
Razi Alexis <razialexis43@gmail.com>
2017-03-10 15:39:01 +0200

    Rewrite subiectul 3, admitere 2011

--- AdmitereUBB/ubb_admitere_2011/sub3.cpp
+++ AdmitereUBB/ubb_admitere_2011/sub3.cpp
@@ -1,90 +1,80 @@
 // http://www.cs.ubbcluj.ro/wp-content/uploads/subiect-admitere-2011-informatica.pdf
-//! Nu se iau doar valorile distincte (To-Do)
 
 #include <iostream>
 
 using namespace std;
 
-bool palindrom(int nr)
+int oglindit(int n)
 {
-    int copie = nr, inv = 0;
-
-    while (nr)
+    int o = 0; // oglinditul (inversul) lui n
+    while (n)
     {
-        inv = inv * 10 + nr % 10;
-        nr /= 10;
+        o = o * 10 + n % 10;
+        n /= 10;
     }
 
-    // Daca numarul este egal cu inversul lui, este palindrom
-    return (copie == inv);
+    return o;
 }
 
-int X[100], n = 0;
-int Y[100], palindroame[100], marimeY = 0, marimeP = -1;
-void citeste()
+bool palindrom(int n)
 {
-    while(cin >> X[n] && X[n] != 0)
-        n++;
+    return n == oglindit(n);
 }
 
-// Vectorul palindroame contine palindroamele nesortate
-// Pentru ca in problema se specifica faptul ca vectorul
-// Y trebuie sa fie construit direct ordonat
-// Cu siguranta ca exista si o metoda mai rapida, si poate
-// Nici n-am avea nevoie de alt vector. Doar ca sunt lenes.
-void construiesteY()
+void insereaza_descrescator(int &n, int *v, int val)
 {
-    int tmp;
-    bool schimbare;
+    for (int i = 0; i < n; ++i)
+        if (v[i] == val)
+            return;
 
-    do
+    int j = n - 1;
+    while (j >= 0 && val > v[j])
     {
-        schimbare = false;
+        v[j + 1] = v[j];
+        --j;
+    }
 
-        for (int i = 0; i < marimeP - 1; ++i)
-            if (palindroame[i] < palindroame[i + 1])
-            {
-                tmp = palindroame[i];
-                palindroame[i] = palindroame[i + 1];
-                palindroame[i + 1] = tmp;
-
-                schimbare = true;
-            }
-    } while (schimbare);
-
-    marimeY = marimeP;
-
-    for (int i = 0; i < marimeY; ++i)
-        Y[i] = palindroame[i];
+    v[j + 1] = val;
+    ++n;
 }
 
-void afisare()
+void citire(int &n, int *v)
 {
-    for (int i = 0; i < marimeY; ++i)
-        cout << Y[i] << ' ';
+    n = 0;
+    int t;
+    cin >> t;
+    while (t != 0)
+    {
+        v[n++] = t;
+        cin >> t;
+    }
+}
+
+void tiparire(int n, int *v)
+{
+    if (n == 0)
+    {
+        cout << "Sirul Y e vid";
+        return;
+    }
+
+    for (int i = 0; i < n; ++i)
+        cout << v[i] << ' ';
+}
+
+void determina_palindroame(int n, int *X, int &m, int *Y)
+{
+    for (int i = 0; i < n; ++i)
+        if (palindrom(X[i]))
+            insereaza_descrescator(m, Y, X[i]);
 }
 
 int main()
 {
-    citeste();
-
-    for (int i = 0; i < n; ++i)
-    {
-        if (palindrom(X[i]))
-        {
-            ++marimeP;
-            palindroame[marimeP] = X[i];
-        }
-    }
-
-    // Nu s-au introdus palindroame
-    if (marimeP == -1)
-        cout << "Sirul Y e vid";
-    else
-    {
-        construiesteY();
-        afisare();
-    }
+    int n, X[100], m = 0, Y[100];
+    citire(n, X);
+    determina_palindroame(n, X, m, Y);
+    tiparire(m, Y);
 
     return 0;
 }
