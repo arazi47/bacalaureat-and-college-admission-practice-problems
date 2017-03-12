9d0424ac7ae5be0b526a23882686524c0a4d58d9
Razi Alexis <razialexis43@gmail.com>
2017-03-12 10:42:04 +0200

    Rewrite subiectul 3, admitere iulie 2014

--- AdmitereUBB/ubb_admitere_iulie_2014/sub3.cpp
+++ AdmitereUBB/ubb_admitere_iulie_2014/sub3.cpp
@@ -1,132 +1,120 @@
 // http://www.cs.ubbcluj.ro/wp-content/uploads/subiect-admitere-2014-iulie-informatica.pdf
+
 #include <iostream>
 
 using namespace std;
 
-int X[1500], m = 1;
-int n;
-int A[10][10];
-
-int PrimaCifra(int n)
+int primacif(int n)
 {
-    int inv = 0;
-    while (n)
-    {
-        inv = inv * 10 + n % 10;
+    while (n > 9)
         n /= 10;
-    }
 
-    return inv % 10;
+    return n;
 }
 
-int UltimaCifra(int n)
+int ultcif(int n)
 {
     return n % 10;
 }
 
-int CifraMaxima(int n)
+int cifmax(int n)
 {
-    int m = 0;
+    int max = 0;
     while (n)
     {
-        if (n % 10 > m)
-            m = n % 10;
+        if (ultcif(n) > max)
+            max = ultcif(n);
 
         n /= 10;
     }
 
-    return m;
+    return max;
 }
 
-void DeterminaX()
+// determina numarul maxim format din elementele de pe coloana "col"
+int determina_max_coloana(int n, int a[10][10], int col)
 {
-    const int nrElem = 2 * n * n * n; // 2 * n^3
+    int v[10] = {};
+    for (int i = 1; i <= n; ++i)
+        ++v[a[i][col]];
 
-    for (int i = 1; m <= nrElem; ++i)
-    {
-        X[m++] = i;
+    int nr = 0;
+    for (int i = 9; i >= 0; --i)
+        while (v[i] > 0)
+        {
+            nr = nr * 10 + i;
+            --v[i];
+        }
 
-        for (int d = 2; d <= i / 2; ++d)
-            if (i % d == 0)
-                X[m++] = d;
-    }
-
-    for (int i = 1; i <= m; ++i)
-        cout << X[i] << ' ';
-    cout << endl << endl;
-
+    return nr;
 }
 
-// Nu merge
-void DeterminaA()
+void determina_sir(int n, int *x)
+{
+    int cnt = 2 * n * n;
+    int m = 1; // pozitia unde am ajuns in x
+    int nr = 1;
+
+    while (m <= cnt)
+    {
+        x[m++] = nr;
+
+        int d = 2;
+        while (d <= nr / 2 && m < cnt)
+        {
+            if (nr % d == 0)
+                x[m++] = d;
+
+            ++d;
+        }
+
+        ++nr;
+    }
+}
+
+void construieste_matrice(int n, int a[10][10], int *x)
+{
+    for (int i = 1; i <= n; ++i)
+        for (int j = 1; j <= n; ++j)
+        {
+            if (i + j < n + 1) // deasupra diagonalei secundare
+                a[i][j] = ultcif(x[n * n + i + j]);
+            else if (i + j > n + 1) // sub diagonala secundara
+                a[i][j] = primacif(x[n * n + i + j]);
+            else // pe diagonala secundara
+                a[i][j] = cifmax(x[n * n + i * i]);
+        }
+}
+
+void afiseaza_matrice(int n, int a[10][10])
 {
     for (int i = 1; i <= n; ++i)
     {
         for (int j = 1; j <= n; ++j)
-        {
-            if (i + j < n + 1) // Deasupra diagonalei secundare
-                A[i][j] = UltimaCifra(X[n * n + i + j]);
-            else if (i + j > n + 1) // Sub diagonala secundare
-                A[i][j] = PrimaCifra(X[n * n + i + j]);
-            else if (i + j == n + 1)
-                A[i][j] = CifraMaxima(X[n * n + i * i]);
+            cout << a[i][j] << ' ';
 
-            cout << A[i][j] << ' ';
-        }
         cout << endl;
     }
-    cout << endl;
 }
 
-int N[100], k;
-void OrdoneazaN()
-{
-    int tmp;
-    bool schimbare;
-
-    do
-    {
-        schimbare = false;
-        for (int i = 0; i < k - 1; ++i)
-        {
-            if (N[i] > N[i + 1])
-            {
-                tmp = N[i];
-                N[i] = N[i + 1];
-                N[i + 1] = tmp;
-                schimbare = true;
-            }
-        }
-    } while (schimbare);
-}
-
-void FormeazaNrMax()
-{
-    OrdoneazaN();
-    int nr = 0;
-    for (int i = k - 1; i >= 0; --i)
-        nr = nr * 10 + N[i];
-
-    cout << nr << ' ';
-}
-
-void DeterminaNrMaxime()
+void afiseaza_numere_maxime(int n, int a[10][10])
 {
     for (int i = 1; i <= n; ++i)
-    {
-        k = 0;
-        for (int j = 1; j <= n; ++j)
-            N[k++] = A[j][i];
-        FormeazaNrMax();
-    }
+        cout << determina_max_coloana(n, a, i) << ' ';
 }
 
 int main()
 {
+    // valoarea maxima a lui n poate fi 9 => 2 * 9 * 9 = 162
+    int n, x[163];
+
     cin >> n;
-    DeterminaX();
-    DeterminaA();
-    DeterminaNrMaxime();
+    determina_sir(n, x);
+
+    int a[10][10];
+    construieste_matrice(n, a, x);
+    afiseaza_matrice(n, a);
+    afiseaza_numere_maxime(n, a);
 
     return 0;
 }
