db56835fca67c4e287b3faf635f1eec024cd3a76
Razi Alexis <razialexis43@gmail.com>
2017-03-12 09:10:28 +0200

    Subiectul 3, admitere 2014

--- AdmitereUBB/ubb_admitere_septembrie_2014/sub3.cpp
+++ AdmitereUBB/ubb_admitere_septembrie_2014/sub3.cpp
@@ -4,45 +4,100 @@
 
 using namespace std;
 
-bool Prim(int n)
+bool prim(int n)
 {
-    if (n == 0 || n == 1)
+    if (n < 2)
         return false;
-
-    for (int d = 2; d * d <= n; ++d)
+    if (n % 2 == 0 && n != 2)
+        return false;
+    for (int d = 3; d * d <= n; d += 2)
         if (n % d == 0)
             return false;
 
     return true;
 }
 
-bool SuperPrim(int n)
+bool superprim(int n)
 {
-    int n2 = n, cifre = 0, putere = 1;
+    if (prim(n))
+        return true;
 
-    while (n2)
+    int c = n, nrcif = 0;
+    while (c)
     {
-        putere *= 10;
-        ++cifre;
-        n2 /= 10;
+        ++nrcif;
+        c /= 10;
     }
 
-    putere /= 10; // Puterea e 10^(numarul cifrelor) acum, dar ne trebuie 10^(numarul cifrelor - 1)
+    --nrcif;
 
-    for (int i = 1; i <= cifre; ++i)
+    int putere = 1;
+    while (nrcif)
     {
-        if (Prim(n))
-            return true;
+        putere *= 10;
+        --nrcif;
+    }
 
-        n = n % putere * 10 + n / putere; // Mutam prima cifra a numarului in pozitia unitatilor
+    c = n;
+    c = (c % putere) * 10 + c / putere;
+    while (c != n)
+    {
+        if (prim(c))
+            return true;
+        c = (c % putere) * 10 + c / putere;
     }
 
     return false;
 }
 
+void interschimba(int a[51][51], int i1, int j1, int i2, int j2)
+{
+    int tmp = a[i1][j1];
+    a[i1][j1] = a[i2][j2];
+    a[i2][j2] = tmp;
+}
+
+void citire(int &n, int a[51][51])
+{
+    cin >> n;
+    for (int i = 1; i <= n; ++i)
+        for (int j = 1; j <= n; ++j)
+            cin >> a[i][j];
+}
+
+void afisare(int n, int a[51][51])
+{
+    for (int i = 1; i <= n; ++i)
+    {
+        for (int j = 1; j <= n; ++j)
+            cout << a[i][j] << ' ';
+        cout << endl;
+    }
+}
+
+void interschimba_superior_inferior(int n, int a[51][51])
+{
+    for (int i = 1; i <= n; ++i)
+        for (int j = 1; j <= n; ++j)
+            if (i < j && i + j < n + 1)
+                interschimba(a, i, j, n + 1 - i, j);
+}
+
+void interschimba_superprime(int n, int a[51][51])
+{
+    for (int i = 1; i <= n; ++i)
+        for (int j = 1; j <= n; ++j)
+            if (i > j && i + j < n + 1)
+                if (superprim(a[i][j]) && superprim(a[i][n + 1 - j]))
+                    interschimba(a, i, j, i, n + 1 - j);
+}
+
 int main()
 {
-
-
+    int n, a[51][51];
+    citire(n, a);
+    interschimba_superior_inferior(n, a);
+    interschimba_superprime(n, a);
+    afisare(n, a);
     return 0;
 }
