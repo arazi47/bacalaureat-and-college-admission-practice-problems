2f841dea982b03780cca4060a68898ceb4b8d3ea
Razi Alexis <razialexis43@gmail.com>
2017-03-14 09:45:18 +0200

    Rewrite subiectul 3, admitere 2012. Cred ca nu e 100% corect tho

--- AdmitereUBB/ubb_admitere_2012/sub3.cpp
+++ AdmitereUBB/ubb_admitere_2012/sub3.cpp
@@ -1,96 +1,125 @@
 // http://www.cs.ubbcluj.ro/wp-content/uploads/subiect-admitere-2012-informatica.pdf
 
+// cred ca la asta mai trebuie lucrat
+
 #include <iostream>
 
 using namespace std;
 
 bool asemenea(int a, int b)
 {
-    int v[10] = {}, m[10] = {};
+    int v[10] = {};
     while (a)
     {
-        v[a % 10]++;
+        v[a % 10] = 1;
         a /= 10;
     }
+
     while (b)
     {
-        m[b % 10]++;
+        ++v[b % 10];
         b /= 10;
     }
 
     for (int i = 0; i < 10; ++i)
-        if ((v[i] > 0 && m[i] == 0) || (v[i] == 0 && m[i] > 0))
+        if (v[i] == 1)
             return false;
 
     return true;
 }
 
-void citeste(int v[], int &n)
+void citire(int &n, int *v)
 {
     cin >> n;
     for (int i = 0; i < n; ++i)
         cin >> v[i];
 }
 
-int gaseste_de_la(int v[], int n, int startI)
+void tiparire(int n, int *v)
 {
-    for (int i = startI; i < n - 1; ++i)
-        if (!asemenea(v[i], v[i + 1]))
-            return i;
+    for (int i = 0; i < n; ++i)
+        cout << v[i] << ' ';
 }
 
-void gaseste_numere_asemenea(int v[], int n, int R[], int &m)
+void determina_secventa_maxima(int n, int *x, int start, int &fin)
 {
+    for (int i = start; i < n; ++i)
+        if (asemenea(x[i], x[i + 1]))
+            ++fin;
+        else
+            break;
+
+    // fin va fi mai mare cu 1 decat indexul final al secventei
+    --fin;
+}
+
+// verificam daca valoarea "val" apartine sirului r
+bool este_in_r(int m, int *r, int val)
+{
+    for (int i = 0; i < m; ++i)
+        if (r[i] == val)
+            return true;
+
+    return false;
+}
+
+void insereaza_in_r(int *x, int &m, int *r, int start, int fin)
+{
+    while (start <= fin)
+    {
+        if (!este_in_r(m, r, x[start]))
+            r[m++] = x[start];
+        ++start;
+    }
+}
+
+void elimina_din_x(int &n, int *x, int start, int fin)
+{
+    // inlocuim elementele situate intre start si fin cu elementele de dupa
+    for (int i = 1; i < n - fin; ++i)
+        x[start++] = x[fin + i];
+}
+
+void determina_secvente(int &n, int *x, int &m, int *r)
+{
+    int fin; // finalul secventei curente
     for (int i = 0; i < n - 1; ++i)
-        if(asemenea(v[i], v[i + 1]))
+    {
+        if (asemenea(x[i], x[i + 1]))
         {
-            int endi = gaseste_de_la(v, n, i);
-
-            if (i != endi)
+            fin = i + 1;
+            determina_secventa_maxima(n, x, i, fin);
+            if (i != fin)
             {
-                for (int k = i; k <= endi; ++k)
-                {
-                    bool ok = true;
-                    for (int q = 0; q < m; ++q)
-                    {
-                        if (R[q] == v[k])
-                        {
-                            ok = false;
-                            break;
-                        }
-                    }
-                    if (ok)
-                        R[m++] = v[k];
-                }
+                // intai inseram in r elementele ce apartin secventei
+                insereaza_in_r(x, m, r, i, fin);
 
-                i = endi;
+                // doar apoi le putem elimina din x
+                elimina_din_x(n, x, i, fin);
+
+                // scade marimea sirului x
+                n -= (fin - i);
             }
         }
-}
-
-void afiseaza(int sir[], int m)
-{
-    if (m == 0)
-    {
-        cout << "R este sirul vid";
-        return;
     }
-
-    for (int i = 0; i < m; ++i)
-        cout << sir[i] << ' ';
 }
 
 int main()
 {
-    int X[100], n;
-    int R[100], m = 0;
+    int n, x[100];
+    int m = 0, r[100];
+    citire(n, x);
 
-    citeste(X, n);
+    determina_secvente(n, x, m, r);
 
-    gaseste_numere_asemenea(X, n, R, m);
-    cout << "X = ";
-    afiseaza(X, n);
+    tiparire(n, x);
+
     cout << endl;
-    cout << "R = ";
-    afiseaza(R, m);
+
+    if (m == 0)
+        cout << "R este sirul vid";
+    else
+        tiparire(m, r);
+
+    return 0;
 }
