90348260167c0b11cb435ddc60dd550c473a6978
Alexis Razi <razialexis43@gmail.com>
2017-07-02 12:58:16 +0300

    Rewrite subiectul 2, admitere UBB 2010, am scris si varianta eficienta a algoritmului de calculare a sumei divizorilor proprii (fara 1 si el insusi)

--- AdmitereUBB/ubb_admitere_2010/sub2.cpp
+++ AdmitereUBB/ubb_admitere_2010/sub2.cpp
@@ -4,47 +4,31 @@
 
 using namespace std;
 
-// Daca e definita urmatoarea variabila, divizorii proprii
-// nu vor fi scazuti din rezultatul final
-#define DIVIZORI_PROPRII
-void SumaDivizorilor()
-{
-    int suma = 1, n, copie;
-    cin >> n;
-    copie = n; // Vom modifica n-ul
-
-	for(int k = 2; k * k <= n; ++k)
-	{
-		int p = 1;
-		while(n % k == 0)
-		{
-			p = p * k + 1;
-			n /= k;
-		}
-
-		suma *= p;
-	}
-
-	if(n > 1)
-		suma *= 1 + n;
-
-#ifndef DIVIZORI_PROPRII
-    // Scadem divizorii proprii
-    suma = suma - 1 - copie;
-#endif
-
-	cout << suma;
-}
-
 // a) Pentru n = 1, se afiseaza 0; pentru n = 10, se afiseaza 7
 // b) pentru(k = 2, n, 1) - k = 2, k se incrementeaza cu 1 pana la n
-// c) Varianta optimizata a algoritmului: @todo (uita-te la functia de mai sus)
+// c) Varianta optimizata a algoritmului
 
-void SubII()
+// subpunctul c
+int SubIIEficient(int n)
 {
-    int n, s = 0, k = 2;
-    cin >> n;
+    int sum = 0;
+    for (int d = 2; d * d <= n; ++d)
+    {
+        if (n % d == 0)
+        {
+            sum += d;
 
+            if (n / d != d)
+                sum += (n / d);
+        }
+    }
+
+    return sum;
+}
+
+int SubII(int n)
+{
+    int s = 0, k = 2;
     while (k < n)
     {
         if (n % k == 0)
@@ -52,12 +36,15 @@ void SubII()
         ++k;
     }
 
-    cout << s;
+    return s;
 }
 
 int main()
 {
-    //SubII();
-    SumaDivizorilor();
+    int n;
+    cin >> n;
+    cout << SubII(n) << ' ' << SubIIEficient(n);
+
     return 0;
 }
+
