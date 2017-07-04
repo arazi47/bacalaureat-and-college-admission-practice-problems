03929b0fbd352be1a692469a6d5a23581c75bf20
Alexis Razi <razialexis43@gmail.com>
2017-07-04 13:09:59 +0300

    Am rescris subiectull 3 de la admiterea de la UBB, septembrie 2015

--- AdmitereUBB/ubb_admitere_septembrie_2015/sub3.cpp
+++ AdmitereUBB/ubb_admitere_septembrie_2015/sub3.cpp
@@ -4,101 +4,130 @@
 
 using namespace std;
 
-void citire(int &n, int *x)
+// numarul maxim de numere din sir
+#define MAX 200
+
+void citire(int &n, int X[MAX])
 {
     cin >> n;
     for (int i = 0; i < n; ++i)
-        cin >> x[i];
+        cin >> X[i];
 }
 
-// verifica daca a si b au cel putin o cifra comuna
-bool cif_comuna(int a, int b)
+bool cifraComuna(int a, int b)
 {
+    // vectorul de frecventa al numarului a
     bool v[10] = {};
     while (a)
     {
-        v[a % 10] = true;
+        if (!v[a % 10])
+            v[a % 10] = true;
+
         a /= 10;
     }
 
     while (b)
     {
-        if (v[b % 10] == true)
+        if (v[b % 10])
             return true;
+
         b /= 10;
     }
 
     return false;
 }
 
-// insereaza pe pozitia "poz" valoarea "val"
-void insereaza(int &n, int *x, int poz, int val)
+// verifica daca elementul de index k din sirul X este perfect
+bool perfect(int n, int X[MAX], int k)
 {
-    for (int i = n; i >= poz; --i)
-        x[i] = x[i - 1];
-
-    x[poz] = val;
-    ++n;
-}
-
-// verifica daca elementul de pe pozitia "poz" este perfect
-bool perfect(int n, int *x, int poz)
-{
-    int i = poz + 1;
-    while (i < n)
-    {
-        if (!cif_comuna(x[poz], x[i]))
+    for (int i = k + 1; i < n; ++i)
+        if (!cifraComuna(X[k], X[i]))
             return false;
-        ++i;
-    }
 
     return true;
 }
 
-void adauga_divizori(int &n, int *x)
+// insereaza in sirul X valoarea val la indexul k
+void insereaza(int &n, int X[MAX], int k, int val)
 {
+    for (int i = n; i > k; --i)
+        X[i] = X[i - 1];
+
+    X[k] = val;
+    ++n;
+}
+
+// adauga in sirul X, divizorii elementului aflat pe indexul k dupa acest element
+void adaugaDivizori(int k, int &n, int X[MAX])
+{
+    for (int d = 2; d <= X[k] / 2; ++d)
+        if (X[k] % d == 0)
+            insereaza(n, X, k + 1, d);
+}
+
+// determina sirul O, ce va contine ordinea fiecarui element din X
+// astfel incat sirul sa fie afisat in ordine descrescatoare
+void determinaOrdineaElementelor(int n, int X[MAX], int O[MAX])
+{
+    for (int i = 0; i < n; ++i)
+        O[i] = i;
+
     for (int i = 0; i < n - 1; ++i)
     {
-        if (perfect(n, x, i))
+        for (int j = i + 1; j < n; ++j)
         {
-            for (int d = 2; d <= x[i] / 2; ++d)
-                if (x[i] % d == 0)
-                    insereaza(n, x, i + 1, d);
+            if (X[O[i]] < X[O[j]])
+            {
+                int tmp = O[i];
+                O[i] = O[j];
+                O[j] = tmp;
+            }
         }
     }
 }
 
-void construieste_o(int n, int *x, int *o)
+// tipareste sirul X in functie de sirul O
+void tipareste(int n, int X[MAX], int O[MAX])
 {
     for (int i = 0; i < n; ++i)
-        o[i] = i;
-
-    for (int i = 0; i < n - 1; ++i)
-        for (int j = i + 1; j < n; ++j)
-            if (x[o[i]] < x[o[j]])
-            {
-                int t = o[i];
-                o[i] = o[j];
-                o[j] = t;
-            }
+        cout << X[O[i]] << ' ';
 }
 
-void afiseaza(int n, int *x, int *o)
+int numarDivizori(int n)
 {
-    for (int i = 0; i < n; ++i)
-        cout << x[i] << ' ';
+    int cnt = 0, d;
+    for (d = 2; d * d < n; d++)
+        if (n % d == 0)
+            cnt += 2;
+
+    if (d * d == n)
+        ++cnt;
+
+    return cnt;
+}
+
+void determinaElementelePerfecte(int &n, int X[MAX])
+{
+    // n - 1 pentru ca ultimul element nu poate fi perfect
+    for (int i = 0; i < n - 1; ++i)
+        if (perfect(n, X, i))
+        {
+            adaugaDivizori(i, n, X);
+
+            // nu-s sigur ca trebuie si asta
+            // sarim peste divizorii introdusi in sir
+            i += numarDivizori(X[i]);
+        }
 }
 
 int main()
 {
-    int n, x[200];
-    citire(n, x);
-
-    adauga_divizori(n, x);
-
-    int o[200];
-    construieste_o(n, x, o);
-    afiseaza(n, x, o);
+    int n, X[MAX], O[MAX];
+    citire(n, X);
+    determinaElementelePerfecte(n, X);
+    determinaOrdineaElementelor(n, X, O);
+    tipareste(n, X, O);
 
     return 0;
 }
+
