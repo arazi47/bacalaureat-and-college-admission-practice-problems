91d0a53f870e069f1d0c8c0b0a61a7c1a6235f02
Alexis Razi <razialexis43@gmail.com>
2017-07-06 01:13:53 +0300

    Am rescris subiectul 3 de la admiterea de la UBB, iulie 2015

--- AdmitereUBB/ubb_admitere_iulie_2015/sub3.cpp
+++ AdmitereUBB/ubb_admitere_iulie_2015/sub3.cpp
@@ -4,6 +4,9 @@
 
 using namespace std;
 
+// + 1 pentru ca incep indexarea matricei de la 1
+#define MAX 100 + 1
+
 bool prim(int n)
 {
     if (n < 2)
@@ -17,11 +20,11 @@ bool prim(int n)
     return true;
 }
 
-// va retuna 10 la puterea numarului de cifre ale lui n
+// returneaza 10 ^ (numarul de cifre ale lui n - 1)
 int putere(int n)
 {
     int p = 1;
-    while (n)
+    while (n > 9)
     {
         p *= 10;
         n /= 10;
@@ -32,98 +35,100 @@ int putere(int n)
 
 bool magic(int n)
 {
-    int n_initial = n;
-    int p = 1;
-    int putere_max = putere(n);
-    while (p != putere_max)
-    {
-        p *= 10;
-        if (!prim(n))
-            return false;
-        n = n_initial % p;
-    }
-
-    putere_max /= 10;
-    // prima cifra
-    if (!prim(n_initial / putere_max))
+    if (!prim(n))
         return false;
+
+    int p = putere(n);
+
+    // prima cifra
+    if (!prim(n / p))
+        return false;
+
+    // eliminam prima cifra, desi nu e neaparat
+    // de exemplu: 3547
+    // 3547 % 100 = 547
+    // oricum nu mai avem treaba cu prima cifra, deci e totuna
+    n %= p;
+
+    while (n % p != 0)
+    {
+        if (!prim(n % p))
+            return false;
+
+        n %= p;
+        p /= 10;
+    }
 
     return true;
 }
 
-void citire(int &n, int &m, int a[101][101])
+void citire(int &n, int &m, int A[MAX][MAX])
 {
     cin >> n >> m;
     for (int i = 1; i <= n; ++i)
         for (int j = 1; j <= m; ++j)
-            cin >> a[i][j];
+            cin >> A[i][j];
 }
 
-void determina_x(int n, int m, int a[101][101], int &l, int *x)
+void construiesteSir(int n, int m, int A[MAX][MAX], int &k, int X[MAX * MAX])
 {
-    l = 1;
+    k = 0;
     for (int i = 1; i <= m; ++i)
         for (int j = 1; j <= n; ++j)
-            x[l++] = a[j][i];
+            X[k++] = A[j][i];
 }
 
-void determina_secventa_maxima(int l, int *x, int st, int &fin)
+void determinaSecventaMaxima(int k, int X[MAX * MAX], int &start, int &stop)
 {
-    for (int i = st; i <= l; ++i)
-        if (magic(x[i] + x[i + 1]))
-            ++fin;
-        else
-            break;
+    int startMax = 0, stopMax = 0;
 
-    // fin se va duce cu o pozitie mai incolo decat trebuie, trebuie sa scadem 1
-    --fin;
-}
-
-void determina_secvente(int l, int *x, int &max_st, int &max_fin)
-{
-    max_st = max_fin = 0; // inceputul si finalul secventei maxime
-    int fin; // finalul secventei curente
-
-    for (int i = 1; i < l; ++i)
+    for (int i = 0; i < k - 1; ++i)
     {
-        if (magic(x[i] + x[i + 1]))
+        if (magic(X[i] + X[i + 1]))
         {
-            fin = i + 1;
-            determina_secventa_maxima(l, x, i, fin);
+            start = i;
+            stop = i + 1;
 
-            if (fin - i > max_fin - max_st)
+            while (magic(X[stop] + X[stop + 1] && stop < k - 1))
+                ++stop;
+
+            if (stop - start > stopMax - startMax)
             {
-                max_st = i;
-                max_fin = fin;
+                startMax = start;
+                stopMax = stop;
             }
+        }
+    }
+
+    start = startMax;
+    stop = stopMax;
+}
+
+void tiparesteSecventaMaxima(int X[MAX * MAX], int start, int stop)
+{
+    if (start == 0 && stop == 0)
+        cout << "Nu exista secventa";
+    else
+    {
+        while (start <= stop)
+        {
+            cout << X[start] << ' ';
+            ++start;
         }
     }
 }
 
-void afiseaza_secventa(int *x, int start, int fin)
-{
-    if (start == 0 || fin == 0)
-    {
-        cout << "Nu exista secventa";
-        return;
-    }
-
-    for (int i = start; i <= fin; ++i)
-        cout << x[i] << ' ';
-}
-
 int main()
 {
-    int n, m, a[101][101];
-    int l, x[101*101]; // l = lungimea sirului x
-
-    citire(n, m, a);
-    determina_x(n, m, a, l, x);
-
-    int start, fin;
-    determina_secvente(l, x, start, fin);
-
-    afiseaza_secventa(x, start, fin);
+    // n, m - dimensiunile matricei A
+    // k - lungimea sirului X (X poate avea maxim n * m elemente, adica MAX * MAX)
+    // start, stop - indexul inceputului si sfarsitului secventei de lungime maxima
+    int n, m, A[MAX][MAX], X[MAX * MAX], k, start, stop;
+    citire(n, m, A);
+    construiesteSir(n, m, A, k, X);
+    determinaSecventaMaxima(k, X, start, stop);
+    tiparesteSecventaMaxima(X, start, stop);
 
     return 0;
 }
+
